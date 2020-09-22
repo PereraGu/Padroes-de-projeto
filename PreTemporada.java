3a3f4cb402c8de56ee3da6bb8633bae0811aa3dd
PereraGu <70782918+PereraGu@users.noreply.github.com>
2020-09-21 21:46:45 -0300

    Delete PreTemporada.java

--- PreTemporada.java
+++
@@ -1,7 +0,0 @@
-public class PreTemporada extends Treinamento {
-    public void atividadeAerobica() {
-      super.atividadeAerobica();
-      super.hidratacao();
-      System.out.println("corrida forte 10 minutos");
-    }
-}

f832fd6bee37323e67b5807cdfd85af8154c14cf
PereraGu <70782918+PereraGu@users.noreply.github.com>
2020-09-21 21:46:35 -0300

    Delete PreJogo.java

--- PreJogo.java
+++
@@ -1,7 +0,0 @@
-public class PreJogo extends Treinamento {
-    public void atividadeAerobica() {
-      super.atividadeAerobica();
-      super.hidratacao();
-      System.out.println("corrida leve 10 minutos");
-    }
-}
